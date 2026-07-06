7de270bab47b70f22de1c6b08f4d93f2f8ed82e2
adritoon <adrian13759@gmail.com>
2026-07-06 10:54:48 -0500

    verce anlyi

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import Script from "next/script";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -54,6 +55,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </main>
         <Footer />
         <WhatsAppButton /> {/* <-- Añadido aquí */}
+        <Analytics />
       </body>
     </html>
   );
